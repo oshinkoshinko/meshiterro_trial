dddd788c6ae9cbe825e150d78c30b3e891e5e427
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2020-12-11 06:23:01 +0000

    [Update] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ae80330c5da80d8f435ed8c88cd0b4668bd7f91fa2cce38333b395280db2da16c620bc1490adcc0b233bbc6cd11ee2a369cc0495dab87757127bd9b88757e1a1'
